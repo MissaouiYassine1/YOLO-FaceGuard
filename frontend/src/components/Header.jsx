51f2db4296f01c44612cd27454d2239d3f5e1d21
Missaoui Yassine <yassine.missaoui@enis.tn>
2025-03-31 05:39:02 +0200

    Ajout d'un logo primaire

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,13 +1,22 @@
+import React from 'react';
 import '../assets/styles/header.css';
+import logo from '../assets/images/logo_YFG.png';
 
 const Header = () => {
   return (
     <header className="app-header">
-      <h1>YOLO-FaceGuard</h1>
+      <div className="logo-container">
+        <img 
+          src={logo} 
+          alt="YOLO-FaceGuard Logo" 
+          className="header-logo"
+        />
+        <h1>YOLO-FaceGuard</h1>
+      </div>
       <nav>
-        <a href="/">Home</a>
-        <a href="/contact">Contact</a>
-        <a href="/results">Results</a>
+        <a href="/"><span>Home</span></a>
+        <a href="/contact"><span>Contact</span></a>
+        <a href="/results"><span>Results</span></a>
       </nav>
     </header>
   );
